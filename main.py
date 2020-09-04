d5a54a99089eb8678c879b342283a619e785f5b9
David Eisenstat <eisenstatdavid@gmail.com>
2020-09-04 13:22:17 -0400

    Display solution

---
+++ main.py
@@ -0,0 +1,31 @@
+import data
+
+
+def export(filename, matrix, scale=72):
+    with open(filename, "w") as f:
+        print(
+            '<svg width="{}" height="{}">'.format(
+                data.width * scale, data.height * scale
+            ),
+            file=f,
+        )
+        for i, row in enumerate(matrix):
+            for j, k in enumerate(row):
+                print(
+                    '<rect x="{}" y="{}" width="{}" height="{}" style="fill:{}" />'.format(
+                        j * scale, i * scale, scale, scale, data.colors[k].rgb
+                    ),
+                    file=f,
+                )
+        print("</svg>", file=f)
+
+
+def main():
+    matrix = [
+        list(range(i * data.width, (i + 1) * data.width)) for i in range(data.height)
+    ]
+    export("arrangement.html", matrix)
+
+
+if __name__ == "__main__":
+    main()
